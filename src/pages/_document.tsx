e80ecc6b9910da1e4746b0c12c6876ebe9b0200e
Gabriel Borges <gbdesigns13@gmail.com>
2022-10-08 15:35:43 -0300

    initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <main className="bg-[#f5f2ef] w-full min-h-screen">
+      <Head>
+        <title>Workwolf</title>
+      </Head>
+      Hello World
+    </main>
+  );
+};
+
+export default Home;
